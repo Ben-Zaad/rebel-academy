5636a9445d4b2159f1c826945e468e493d6df8a4
BenZaad <75256474+Ben-Zaad@users.noreply.github.com>
2021-08-04 12:30:48 +0300

    Update vehicles-data.js

--- src/vehicles/vehicles-data.js
+++ src/vehicles/vehicles-data.js
@@ -1,11 +1,15 @@
 import React, { useState, useEffect }  from 'react';
+import styled from 'styled-components';
+import ClipLoader from "react-spinners/ClipLoader";
 
-import { GetMultiplePagesData } from '../api/get-multiple-pages-data';
 import { getMultiplePagesData } from '../api/get-multiple-pages-data-noreact';
 
 
 export function Vehicles({ callback }) {
+  const [isDone, setIsDone] = useState(false)
+
   const filterPilotlessVehicles = (data) => {
+    setIsDone(true);
     const filterdData = data.filter(vehicles => vehicles.pilots.length > 0);
     callback(filterdData);
   }
@@ -16,10 +20,25 @@ export function Vehicles({ callback }) {
 
   return (
     <div>
-      {/* <GetMultiplePagesData
-        url='https://swapi.dev/api/vehicles'
-        callback={filterPilotlessVehicles}
-      /> */}
-    </div>
+     <Container>
+      {!isDone && 
+      <LoaderDiv><ClipLoader color={'White'} loading={true}  size={50} /><Loading>Loading Vehicles Data...</Loading></LoaderDiv>
+      }
+    </Container>
+  </div>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const LoaderDiv = styled.div`
+  display: flex;
+`;
+
+const Loading = styled.h2`
+  color: white;
+`
